71fd4ea3f3f75223b5884c3f81ff7fdd5cb726f3
datastore24 <co@datastore24.ru>
2015-12-19 17:01:57 +0300

    New Project

---
+++ HappyParents/HappyParents/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HappyParents
+//
+//  Created by Кирилл Ковыршин on 19.12.15.
+//  Copyright © 2015 datastore24. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HappyParents/Pods/Target Support Files/Pods-HappyParents/Pods-HappyParents-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HappyParents : NSObject
+@end
+@implementation PodsDummy_Pods_HappyParents
+@end
