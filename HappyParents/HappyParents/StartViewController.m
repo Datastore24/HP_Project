272de951b2d6e7e898d9aec04c5059c36fa3feae
Viktor Mishustin <mrgyugo@gmail.com>
2015-12-23 00:29:10 +0300

    Работа с введением

--- HappyParents/HappyParents/StartViewController.m
+++ HappyParents/HappyParents/StartViewController.m
@@ -6,13 +6,13 @@
 //  Copyright © 2015 datastore24. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "StartViewController.h"
 
-@interface ViewController ()
+@interface StartViewController ()
 
 @end
 
-@implementation ViewController
+@implementation StartViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
